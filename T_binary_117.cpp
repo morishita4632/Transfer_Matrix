7bffcaccc91e26f3ee22ac9d6b3b32d1bbbeea16
morishita <t.m.thp.hnk@gmail.com>
2020-11-25 23:12:32 +0900

    .

--- T_binary_117.cpp
+++ T_binary_117.cpp
@@ -1,35 +1,34 @@
 #include "Triangular.h"
 
-int main()
-{
+int main() {
   START();
 
-  int M1 = 18, M2 = M1 + 1;
+  int M1 = 4, M2 = M1 + 1;
   double Js[3] = {1.0, 1.0, 7.0};
   double EPS = 1e-12;
-  double l = 0.51183704507, r = 0.51183958242, c;
+  double L = 0.3, R = 0.6;
 
   Triangular T1(Js, M1, EPS);
-  double *vo_1 = alloc_dvector(T1.dim);
-  double *vn_1 = alloc_dvector(T1.dim);
-  double *vtmp1_1 = alloc_dvector(T1.dimt);
-  double *vtmp2_1 = alloc_dvector(T1.dimt);
-  double *v1R_1 = alloc_dvector(T1.dim);
-  double *v1L_1 = alloc_dvector(T1.dim);
-  double *v2R_1 = alloc_dvector(T1.dim);
+  double* vo_1 = alloc_dvector(T1.dim);
+  double* vn_1 = alloc_dvector(T1.dim);
+  double* vtmp1_1 = alloc_dvector(T1.dimt);
+  double* vtmp2_1 = alloc_dvector(T1.dimt);
+  double* v1R_1 = alloc_dvector(T1.dim);
+  double* v1L_1 = alloc_dvector(T1.dim);
+  double* v2R_1 = alloc_dvector(T1.dim);
 
   Triangular T2(Js, M2, EPS);
-  double *vo_2 = alloc_dvector(T2.dim);
-  double *vn_2 = alloc_dvector(T2.dim);
-  double *vtmp1_2 = alloc_dvector(T2.dimt);
-  double *vtmp2_2 = alloc_dvector(T2.dimt);
-  double *v1R_2 = alloc_dvector(T2.dim);
-  double *v1L_2 = alloc_dvector(T2.dim);
-  double *v2R_2 = alloc_dvector(T2.dim);
+  double* vo_2 = alloc_dvector(T2.dim);
+  double* vn_2 = alloc_dvector(T2.dim);
+  double* vtmp1_2 = alloc_dvector(T2.dimt);
+  double* vtmp2_2 = alloc_dvector(T2.dimt);
+  double* v1R_2 = alloc_dvector(T2.dim);
+  double* v1L_2 = alloc_dvector(T2.dim);
+  double* v2R_2 = alloc_dvector(T2.dim);
 
   double xi1, xi2;
-  while (r - l > EPS)
-  {
+  double l = L, r = R, c;
+  while (r - l > EPS) {
     c = (l + r) / 2.0;
     xi1 = T1.calc_xi(c, vo_1, vn_1, vtmp1_1, vtmp2_1, v1R_1, v1L_1, v2R_1);
     xi2 = T2.calc_xi(c, vo_2, vn_2, vtmp1_2, vtmp2_2, v1R_2, v1L_2, v2R_2);
@@ -45,7 +44,17 @@ int main()
     Tc = -1.0;
 
   printf("J = (%.1f, %.1f, %.1f)\n", Js[0], Js[1], Js[2]);
-  printf("M = %d, %d\n%.11f\n", M1, M2, Tc);
+  printf("M = %d, %d\n%.11f\n\n", M1, M2, Tc);
+
+  if (Tc != -1.0) {
+    // increase
+    double next_L = c, next_R = 2 * c - L;
+    printf("Inc\ndouble L = %.11f, R = %.11f;\n", next_L, next_R);
+
+    // decrease
+    next_R = c, next_L = 2 * c - R;
+    printf("Dec\ndouble L = %.11f, R = %.11f;\n", next_L, next_R);
+  }
 
   END();
 }
