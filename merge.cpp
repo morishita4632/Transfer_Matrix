6067dd273fb03d867235d0af3241c5bb5a462ac9
morishita <t.m.thp.hnk@gmail.com>
2020-11-27 02:11:43 +0900

    1_1_(1,2) - merge

--- merge.cpp
+++ merge.cpp
@@ -8,7 +8,7 @@ using namespace boost;
 
 int main() {
   string lattice = "Triangular";
-  string Js = "1_1_500";
+  string Js = "1_1_2";
   int id_min = 80000, id_max = 90000;
 
   string dir = (format("./out/%s/%s/") % lattice % Js).str();
