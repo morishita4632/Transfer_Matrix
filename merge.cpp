b9bcd81e4d7b515793041fe941cc4a5407728d3f
morishita <t.m.thp.hnk@gmail.com>
2020-11-26 21:22:06 +0900

    1_1_1_50 - merge

--- merge.cpp
+++ merge.cpp
@@ -8,7 +8,7 @@ using namespace boost;
 
 int main() {
   string lattice = "Xsquare";
-  string Js = "5_5_1_1";
+  string Js = "1_1_1_50";
   int id_min = 80000, id_max = 90000;
 
   string dir = (format("./out/%s/%s/") % lattice % Js).str();
