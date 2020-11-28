e146732f7335d13663b362eef1abdc6c33af48cd
morishita <t.m.thp.hnk@gmail.com>
2020-11-28 22:08:52 +0900

    fix λ2 (X)

--- X_xi.cpp
+++ X_xi.cpp
@@ -13,15 +13,12 @@ int main() {
   double* vn = alloc_dvector(X.dim);
   double* vtmp1 = alloc_dvector(X.dimt);
   double* vtmp2 = alloc_dvector(X.dimt);
-  double* v1R = alloc_dvector(X.dim);
-  double* v1L = alloc_dvector(X.dim);
-  double* v2R = alloc_dvector(X.dim);
+  double* v1 = alloc_dvector(X.dim);
+  double* v2 = alloc_dvector(X.dim);
 
-  double lmd1 = X.power1_R(temperature, vo, vn, v1R, vtmp1, vtmp2);
-  X.power1_L(temperature, vo, vn, v1L, vtmp1, vtmp2);
-  double lmd2 =
-      X.power2(temperature, lmd1, vo, vn, v1R, v1L, vtmp1, vtmp2, v2R);
-  double xi = X.calc_xi(temperature, vo, vn, vtmp1, vtmp2, v1R, v1L, v2R);
+  double lmd1 = X.power1(temperature, vo, vn, v1, vtmp1, vtmp2);
+  double lmd2 = X.power2(temperature, lmd1, vo, vn, v1, vtmp1, vtmp2, v2);
+  double xi = X.calc_xi(temperature, vo, vn, vtmp1, vtmp2, v1, v2);
 
   printf("λ1 = %.12f\n", lmd1);
   printf("λ2 = %.12f\n", lmd2);
