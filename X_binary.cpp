ff6ddca5fbc2c9c469d85c7b51ec1c58f2e86cbd
Tomoyuki Morishita <k001020@ohtaka1.issp.u-tokyo.ac.jp>
2020-11-29 03:08:27 +0900

    1_1_10_100 calc

--- X_binary.cpp
+++ X_binary.cpp
@@ -4,7 +4,7 @@ int main() {
   for (int M = 4; M <= 16; M++) {
     START();
     int M1 = M, M2 = M1 + 1;
-    double Js[4] = {1.0, 1.0, 1.0, 1.0};
+    double Js[4] = {1.0, 1.0, 1.0, 20.0};
     double EPS = 1e-12;
     double L = 0.2, R = 0.7;
 
