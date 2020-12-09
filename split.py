daa7cef64038df1231dad09073096e449d4f8a0a
morishita <t.m.thp.hnk@gmail.com>
2020-12-10 00:20:30 +0900

    .

--- split.py
+++ split.py
@@ -1,11 +1,8 @@
 import glob, os
-lattice = "Triangular"
-id = "100984"
 
-# rfile = "out/" + lattice + '/' + "slurm-" + id + ".out"
 rfile = "out/Triangular/slurm_smaller-epsilon-101653.out" 
 line = 0
-Js_str = "hoge"
+Js_str = ""
 
 with open(rfile) as rf:
     for s_line in rf:
