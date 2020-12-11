a28a5b9430cc9e16e163982ea0b5538e78fa27dd
morishita <t.m.thp.hnk@gmail.com>
2020-12-12 00:01:32 +0900

    fix X for odd only

--- X_binary.cpp
+++ X_binary.cpp
@@ -2,16 +2,15 @@
 #include "Xsquare.hpp"
 
 int main() {
-  vector<vector<double>> Js_s = {{0, 1e-3, 1, 1}};
+  vector<vector<double>> Js_s = {{2, 2, 1, 1}};
 
   for (auto Js_vec : Js_s) {
-    for (int M = 11; M <= 11; M++) {
+    for (int M = 5; M <= 13; M += 2) {
       START();
       int M1 = M, M2 = M1 + 2;
-      // double Js[4] = {2, 2, 1, 1};
       double Js[4] = {Js_vec[0], Js_vec[1], Js_vec[2], Js_vec[3]};
       double EPS = 1e-12;
-      double L = 0.5, R = 0.72;
+      double L = 0.2, R = 0.8;
 
       Xsquare X1(Js, M1, EPS);
       double* vo_1 = alloc_dvector(X1.dim);
@@ -44,13 +43,6 @@ int main() {
       printf("J = (%.1f, %.1f, %.1f, %.1f)\n", Js[0], Js[1], Js[2], Js[3]);
       printf("M = %d, %d\n%.11f\n\n", M1, M2, Tc);
 
-      // // increase
-      // double next_L = c, next_R = 2 * c - L;
-      // printf("Inc\n  double L = %.11f, R = %.11f;\n", next_L, next_R);
-
-      // // decrease
-      // next_R = c, next_L = 2 * c - R;
-      // printf("Dec\n  double L = %.11f, R = %.11f;\n", next_L, next_R);
       // END();
     }
   }
