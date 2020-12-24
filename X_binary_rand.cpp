f76102a561958515d4c392d22573d6933278b804
morishita <t.m.thp.hnk@gmail.com>
2020-12-25 03:51:28 +0900

    Fix X_binary_rand (split save file)

--- X_binary_rand.cpp
+++ X_binary_rand.cpp
@@ -7,13 +7,18 @@ int main() {
   START(seed);
 
   int num = 20;
-
+  int Js_range[4] = {1,100,1,1};
+  
   int M_start = 3, M_end = 15;
-
+  char buff_Js[128], buff_Js_raw[128], buff_Tc[128];
+  sprintf (buff_Js, "./out/Xsquare/%d_%d_%d_%d_Js.txt", Js_range[0], Js_range[1], Js_range[2], Js_range[3]);
+  sprintf (buff_Js_raw, "./out/Xsquare/%d_%d_%d_%d_Js_raw.txt", Js_range[0], Js_range[1], Js_range[2], Js_range[3]);
+  sprintf (buff_Tc, "./out/Xsquare/%d_%d_%d_%d_Tc.txt", Js_range[0], Js_range[1], Js_range[2], Js_range[3]);
+  
   FILE* fp;
   while (num-- > 0) {
     chrono_start = chrono::system_clock::now();
-    double Js[4] = {rand01(), rand01()*100, rand01(), rand01()};
+    double Js[4] = {rand01()*Js_range[0], rand01()*Js_range[1], rand01()*Js_range[2], rand01()*Js_range[3]};
     for (int M = M_start; M <= M_end; M += 2) {
       int M1 = M, M2 = M1 + 2;
       double EPS = 1e-12;
@@ -52,18 +57,16 @@ int main() {
         for (int i = 0; i < 4; i++)
           printf("%.2f%c", Js[i], (i == 3 ? '\n' : ' '));
 
-        fp = fopen("./out/Xsquare/Js_rand.txt", "a");
+        fp = fopen(buff_Js, "a");
         for (int i = 0; i < 4; i++)
           fprintf(fp, "%.12f%c", X1.Js[i], (i == 3 ? '\n' : ' '));
         fclose(fp);
-
-        fp = fopen("./out/Xsquare/Js_raw_rand.txt", "a");
+        fp = fopen(buff_Js_raw, "a");
         for (int i = 0; i < 4; i++)
           fprintf(fp, "%.2f%c", Js[i], (i == 3 ? '\n' : ' '));
         fclose(fp);
       }
-
-      fp = fopen("./out/Xsquare/Tc_rand.txt", "a");
+      fp = fopen(buff_Tc, "a");
       fprintf(fp, "%.12f%c", Tc, (M == M_end ? '\n' : ' '));
       fclose(fp);
     }
